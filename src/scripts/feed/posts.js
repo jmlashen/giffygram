458f7ef7876112e68ed7431dfcecf65eec649163
Jake Lashenik <jmlashen@gmail.com>
2021-09-21 08:47:22 -0500

    JS for edit and cancel buttons

--- src/scripts/feed/posts.js
+++ src/scripts/feed/posts.js
@@ -1,23 +1,26 @@
 
-  // export const Post = (postObject) => {
-  //   return `
-  //     <section class="post">
-  //       <header>
-  //           <h2 class="post__title">${postObject.title}</h2>
-  //       </header>
-  //       <img class="post__image" src="${postObject.imageURL}" />
-  //     </section>
-  //   `
-  // }
+// export const Post = (postObject) => {
+//   return `
+//     <section class="post">
+//       <header>
+//           <h2 class="post__title">${postObject.title}</h2>
+//       </header>
+//       <img class="post__image" src="${postObject.imageURL}" />
+//     </section>
+//   `
+// }
 
-  export const Post = (postObject) => {
-    return `
+export const Post = (postObject) => {
+  return `
       <section class="post">
         <header>
             <h2 class="post__title">${postObject.title}</h2>
         </header>
         <img class="post__image" src="${postObject.imageURL}" />
-        <div><button id="edit--${postObject.id}">Edit</button></div>
+        <button id="edit__${postObject.id}">Edit</button>
+        <div id="buttonMove"><button id="delete--${postObject.id}">Delete</button></div>
+
+
       </section>
     `
 }
